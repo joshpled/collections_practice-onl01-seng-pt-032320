6abbfcd0f5dca5eeed7fbcf22c359fc9e59a8f98
Joshua Perez Leduc <joshuapleduc@gmail.com>
2020-04-03 04:39:57 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -41,5 +41,5 @@ def sum_array(array)
 end
 
 def add_s(array)
-  array.each_with_index.collect{|element, index| index != 2 ? element + 's' : }
+  array.each_with_index.collect{|element, index| element + 's' ? index != 2 }
 end
